18065b115017a6ed2eecf9d6371a39916634c976
Kaique Garcia Menezes <git@kaiquegarcia.dev>
2025-04-08 18:16:53 -0300

    fix: add db skip rules to Student entity

--- internal/entity/student.go
+++ internal/entity/student.go
@@ -3,7 +3,7 @@ package entity
 import "time"
 
 type Student struct {
-	ID            int       `db:"ID"`
+	ID            int       `db:"ID;skip-insert;skip-update"`
 	InstitutionID int       `db:"institutionID"`
 	Login         string    `db:"login"`
 	Email         string    `db:"email"`
@@ -12,5 +12,5 @@ type Student struct {
 	Iam           int       `db:"Iam"`
 	Status        int       `db:"status"`
 	UpdatedAt     time.Time `db:"status_date" layout:"2006-01-02 15:04:05"`
-	CreatedAt     time.Time `db:"registry_date" layout:"2006-01-02 15:04:05"`
+	CreatedAt     time.Time `db:"registry_date;skip-insert;skip-update" layout:"2006-01-02 15:04:05"`
 }
